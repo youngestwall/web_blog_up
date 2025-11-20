4e9c91c847578f57c0f036d4a2d50f72e36628af
ypn48694062 <phainie03@gmail.com>
2025-11-20 11:14:29 +0700

    Fix PayloadTooLargeError - Increase body-parser limit to 50mb

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
